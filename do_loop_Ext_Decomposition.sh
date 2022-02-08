ed95043055ce37387f1bbfbd552917c486bd65d2
BobWang@UH <65426380+bob-Github-2020@users.noreply.github.com>
2022-02-08 15:28:30 -0600

    Update do_loop_Ext_Decomposition.sh

--- do_loop_Ext_Decomposition.sh
+++ do_loop_Ext_Decomposition.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
-## 1-27-2022, loop do_plot_water.py
+## 1-27-2022, loop Extensometer_Decomposition.py for processing All USGS Ext. data
+## get list of all TABLE*.col file
 
-##get list of all *.col file
 for file in `ls TABLE*.col`; do
 echo $file > process.ctl
-#./do_plot_Extensometer.py
 ./Extensometer_Decomposition.py
-##rm $file
 done
 
-
-####generate Houston_psveloH.txt
-#for file in `ls *psveloH.txt`;do
-#cat $file >> Houston_psveloH.txt
-#done
-####generate Houston_psveloV.txt
-#for file in `ls *psveloV.txt`;do
-#cat $file >> Houston_psveloV.txt
-#done
