3224bc075140cd686bccfe35b3da80a295298396
BobWang@UH <65426380+bob-Github-2020@users.noreply.github.com>
2022-02-09 13:26:44 -0600

    Update do_loop_Ext_Decomposition.sh

--- do_loop_Ext_Decomposition.sh
+++ do_loop_Ext_Decomposition.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ## 1-27-2022, loop Extensometer_Decomposition.py for processing All USGS Ext. data
-## get list of all TABLE*.col file
+## get list of all TABLE*.txt files, USGS raw data
 
-for file in `ls TABLE*.col`; do
+for file in `ls TABLE*.txt`; do
 echo $file > process.ctl
 ./Extensometer_Decomposition.py
 done
